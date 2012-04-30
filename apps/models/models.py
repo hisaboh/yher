4bf1d3339756f175bf18a9a6bd4de178348666f2
hisaboh <hisaboh@gmail.com>
2012-04-30 20:28:16 +0900

    Projectモデルを追加

--- apps/models/models.py
+++ apps/models/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(blank=False, max_length=200)
+    description = models.TextField(blank=True)
+    velocity = models.IntegerField(blank=False, null=False, default=8)
+    iteration_term = models.IntegerField(blank=True, null=True, default=14)
+    created_at = models.DateTimeField(blank=False, auto_now_add=True)
+    update_at = models.DateTimeField(blank=False, auto_now_add=True)
