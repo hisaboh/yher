194b8f3ed48e5451d23bae4ca6a480bcc89d9f2b
hisaboh <hisaboh@gmail.com>
2012-04-30 23:07:26 +0900

    SITE_IDエラー対策
    正しいSITE_ID をsettings.pyに設定すればなおるはず。
    
    ```python:settings.py
    SITE_ID = u'XXXXXXXXXXXXXXXXX'
    ```
    
    と思ったが、結局test時にこのエラーが出ることは押さえられなかった。
    
    原因はdjango/contrib/sites/management.py。
    ここでSite作成時に強制的にpk=1を入れているため。
    
    ```python:django/contrib/sites/management.py
    def create_default_site(app, created_models, verbosity, db, **kwargs):
        # Only create the default sites in databases where Django created
    the table
        if Site in created_models and router.allow_syncdb(db, Site) :
            if verbosity >= 2:
                print "Creating example.com Site object"
            # The default settings set SITE_ID = 1, and some tests in
    Django's test
            # suite rely on this value. However, if database sequences are
    reused
            # (e.g. in the test suite after flush/syncdb), it isn't
    guaranteed that
            # the next id will be 1, so we coerce it. See #15573 and
    #16353. This
            # can also crop up outside of tests - see #15346.
            s = Site(pk=1, domain="example.com", name="example.com")
            s.save(using=db)
        Site.objects.clear_cache()
    
    signals.post_syncdb.connect(create_default_site, sender=site_app)
    ```
    
    面倒なので、INSTALL_APPSでdjango.contrib.authとdjango.contrib.sitesを使うのをやめた。
    
    ```python:settings.py
    INSTALLED_APPS = (
        #'django.contrib.auth',
        'django.contrib.contenttypes',
        'django.contrib.sessions',
        #'django.contrib.sites',
        'django.contrib.messages',
        'django.contrib.staticfiles',
        # Uncomment the next line to enable the admin:
        # 'django.contrib.admin',
        # Uncomment the next line to enable admin documentation:
        # 'django.contrib.admindocs',
    )
    ```

--- settings.py
+++ settings.py
@@ -110,10 +110,10 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
+    #'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
